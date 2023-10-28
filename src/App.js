810b19f7616d1540c3d229ce112b69c0dba0e752
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-10-28 07:49:40 +0600

    Hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero/>
     </div>
   );
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <>
+      <div className="hero-section">
+        <div className="hero-content">
+          <h1>We provide all class room with free breakfast </h1>
+          <p>What are you waiting for!</p>
+          <a className="btn btn-warning p-2" href="#">
+            Get Started
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
