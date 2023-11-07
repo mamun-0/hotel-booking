06f58e910c2685ff27ae1414aad0fbe8078534be
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-11-07 09:51:37 +0600

    show signup and signin page route inside App.js and setup redux functionality in index.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Navbar from "./Components/Navbar/Navbar";
 import Standard from "./Components/Pages/Standard/Standard";
 import Executive from "./Components/Pages/Executive/Executive";
 import Deluxe from "./Components/Pages/Deluxe/Deluxe";
+import Login from "./Components/Pages/Login/Login";
+import SignUp from "./Components/Pages/SignUp/SignUp";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <Hero />
+        <Route exact path="/" component={Hero} />
         <Route exact path="/standard" component={Standard} />
         <Route exact path="/executive" component={Executive} />
         <Route exact path="/deluxe" component={Deluxe} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
       </BrowserRouter>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducers from "./reducer";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider
+    store={createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}
+  >
+    <App />
+  </Provider>
+);
